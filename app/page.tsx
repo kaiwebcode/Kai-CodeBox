f9a02a1ad98d0b72e73b5c5024fae44bff9191b3
kaiwebcode <kaifq1508@example.com>
2025-11-27 15:17:51 +0530

    Chapter 1 and 2 Project Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+/**
+ * Default Home Page for Kai-CodeBox
+ * @returns 
+ */
+ 
 export default function Home() {
   return (
    <div>
