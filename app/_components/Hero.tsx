46d15df67573fccff50b1c44997a2c5c48f96714
kaiwebcode <kaifq1508@example.com>
2025-12-07 14:35:17 +0530

    chapter 3 and 4 along with landing page

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -14,17 +14,25 @@ function Hero() {
         className="w-full h-full object-cover absolute inset-0 "
       />
 
-      <div className="absolute w-full flex flex-col items-center justify-center mt-24 ">
+      <div className="absolute w-full flex flex-col items-center justify-center mt-32 ">
         <h2 className="font-bold text-7xl font-game">Start Your</h2>
         <h2
           className="font-bold text-8xl font-game text-yellow-400"
           style={{
             textShadow:
               "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000",
-          }}
+          }} 
         >
           Coding Adventure
         </h2>
+
+        <h2 className="mt-5 font-game text-3xl">
+          Beginner friendly coding challenges to level up your skills!
+        </h2>
+
+        <Button className="font-game text-3xl p-5 mt-7" variant={"pixel"}>
+          GET STARTED
+        </Button>
       </div>
     </div>
   );
